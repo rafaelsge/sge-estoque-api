8ca48fd2f59d92e45e5c134eed039fcca290870d
SGE ERP <rafael@sgeerp.com.br>
2026-02-10 11:41:31 -0300

    Ajustes Payload de cadastro02

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ import { setupSwagger } from './swagger'; //
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 app.use('/auth', authRoutes);
 app.use('/sync', syncRoutes);
